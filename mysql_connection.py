24c5b05f3cdf8df8aaf17dbb607c5d20f33950dc
cpdjvks <151480550+cpdjvks@users.noreply.github.com>
2023-12-05 17:41:40 +0900

    add

---
+++ mysql_connection.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+# 파이썬으로 MySQL에 접속할 수 있는 함수.
+def get_connection() :
+    connection = mysql.connector.connect(
+        host = 'yhdb.cezffzy8hpnc.ap-northeast-2.rds.amazonaws.com',
+        database = 'recipe_db',
+        user = 'recipe_db_user',
+        password = '1234'
+    )
+    return connection
